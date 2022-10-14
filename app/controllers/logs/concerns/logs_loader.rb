bf0895f2e45c968843cf81a2d3342506307988cb
Tonyvoon <tonyvoon@dreamcode.solutions>
2022-10-14 22:03:32 +0800

    fix

--- app/controllers/logs/concerns/logs_loader.rb
+++ app/controllers/logs/concerns/logs_loader.rb
@@ -9,12 +9,11 @@ module Logs
         before_action :load_logs
         before_action :load_logs_list
 
-        def load_logs
-          LOG_PATH = Rails.root + 'log/'
+        def load_logs          
           
           file_name = params[:name] || Rails.env
 
-          if File.exist?(LOG_PATH + (file_name + '.log'))
+          if File.exist?(Rails.root + 'log/' + (file_name + '.log'))
             @logs = Logs::Viewer.call(file_name).read
           else
             @logs = nil
